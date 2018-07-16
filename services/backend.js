51b94f2c44880aa4a70c8e7fd5cb08e6631e2ae3
Will Kent-Daggett <willkentdaggett@gmail.com>
2018-07-16 13:25:42 -0700

    use dotenv for handling variables

--- services/backend.js
+++ services/backend.js
@@ -13,6 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+require('dotenv').config();
 
 const fs = require('fs');
 const Hapi = require('hapi');
