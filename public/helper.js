a6b06482310567ceecfe567132eb8e97c8ad4680
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2018-09-20 09:11:45 -0700

    Always include both online and local twitch-ext (#19)
    The Developer Rig's local implementation of twitch-ext will activate
    itself only in local mode.  This obviates the need for separate local
    and online files.  The Developer Rig will no longer attempt to copy
    files from sub-directories.

---
+++ public/helper.js
@@ -0,0 +1,9 @@
+window.addEventListener('message', (event) => {
+  console.log(event);
+});
+//debugger;
+//const local = window.frameElement.attributes.getNamedItem('helperUrl');
+//const online = 'https://extension-files.twitch.tv/helper/v1/twitch-ext.min.js';
+//const el = document.createElement('script');
+//el.src = local ? local.value : online;
+//document.head.appendChild(el);
